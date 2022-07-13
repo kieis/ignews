f54702580ec9a34efb7a1712e69c7896844ea9ca
Dirlan Ferreira <dirlan2014@yahoo.com.br>
2022-07-13 13:31:14 -0300

    server-side rendering to static site generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import styles from './home.module.scss';
 import { SubscribeButton } from '../components/SubscribeButton';
@@ -31,7 +31,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1LL72KDitAZRUpLVvstM14BM', {
     expand: ['product']
   });
@@ -46,7 +46,8 @@ export const getServerSideProps: GetServerSideProps = async () => {
   
   return {
     props: {
-      product
-    }
+      product,
+    },
+    revalidate: 60 * 60 * 24, //24 hours
   }
 };
