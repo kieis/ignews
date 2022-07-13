19f6860aa0132e62fa55e69d4389ac488f63e73a
Dirlan Ferreira <dirlan2014@yahoo.com.br>
2022-07-13 13:21:32 -0300

    upload

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import { SignInButton } from '../SignInButton/index';
+import styles from './styles.module.scss'
+
+export function Header() {
+    return (
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <img src="/images/logo.svg" alt="Logo" />
+                <nav>
+                    <a className={styles.active}>Home</a>
+                    <a>Posts</a>
+                </nav>
+                <SignInButton />
+            </div>
+        </header>
+    );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,52 @@
+import { GetServerSideProps } from 'next';
+import Head from 'next/head';
+import styles from './home.module.scss';
+import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
+
+interface HomeProps {
+  product: {
+    priceId: string,
+    amount: number
+  }
+}
+
+export default function Home({ product }: HomeProps) {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>Get access to all the publications <br/><span>for {product.amount} month</span></p>
+          <SubscribeButton priceId={product.priceId} />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1LL72KDitAZRUpLVvstM14BM', {
+    expand: ['product']
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100)
+  };
+  
+  return {
+    props: {
+      product
+    }
+  }
+};
